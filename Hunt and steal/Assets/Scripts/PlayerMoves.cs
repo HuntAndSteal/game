d0442cc1d0a48e7360f90186a8dfc31170bc19d1
Ange Konian <ange-marie-adingra.konian@epita.fr>
2021-10-24 11:48:11 +0200

    moves up

--- Hunt and steal/Assets/Scripts/PlayerMoves.cs
+++ Hunt and steal/Assets/Scripts/PlayerMoves.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
@@ -9,7 +10,8 @@ public class PlayerMoves : MonoBehaviour
 {
     PhotonView view;
     [SerializeField] private CinemachineVirtualCamera cam;
-    private float speed = 2.0f;
+    [SerializeField]private float speed = 6.0f;
+    public CharacterController controller;
 
     public GameObject player;
     // Start is called before the first frame update
@@ -20,17 +22,25 @@ public class PlayerMoves : MonoBehaviour
         {
             cam = FindObjectOfType<CinemachineVirtualCamera>();
             cam.Follow = gameObject.transform.Find("Follow target");
+            controller = gameObject.AddComponent<CharacterController>();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
+            
         if (view.IsMine)
         {
-            if (Input.GetKey((KeyCode.RightArrow)))
+            if (direction.magnitude >=0.1f)
             {
-                transform.position += Vector3.right * speed * Time.deltaTime;
+                float targetAngle = Mathf.Atan2(direction.x, direction.z)*Mathf.Rad2Deg;
+                transform.rotation=Quaternion.Euler(0f,targetAngle,0f);
+                controller.Move(direction * speed * Time.deltaTime);
             }
         }
     }
